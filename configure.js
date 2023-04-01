4eac5f4b36dfecb95580d9aa2b69680866f69467
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2023-04-01 12:51:16 +0400

    migrate to @backkit/autoconf

--- configure.js
+++ configure.js
@@ -1,66 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const yaml = require('js-yaml');
+const autoconf = require("@backkit/autoconf");
 
-const skipPrompt = process.env.NO_INTERACTIVE || process.env.NO_PROMPT ? true : false;
-const skipAutoconf = process.env.NO_AUTOCONF ? true : false;
-
-const generate = (serviceName, moduleName, config) => {
-  const serviceDir = `${__dirname}/../../services`;
-  const servicePath = `${__dirname}/../../services/${serviceName}.js`;
-  const configDir = `${__dirname}/../../config`;
-  const configPath = `${__dirname}/../../config/${serviceName}.yml`;
-
-  console.log("");
-  console.log(`${serviceName} service config:`);
-  console.log(JSON.stringify(config, null, '  '));
-  console.log("");
-
-  // save service config
-  console.log(`writing config: ${configPath}`);
-  if (!fs.existsSync(configDir)) {
-    fs.mkdirSync(configDir, {recursive: true});
+autoconf('authorize')
+.generator(self => ([
+  {
+    putFileOnce: self.serviceConfigMainYML,
+    contentYml: self.config
+  },
+  {
+    putFileOnce: self.serviceCodeMainJS,
+    content: `module.exports = require('${self.npmModuleName}')`
   }
-  if (!fs.existsSync(configPath)) {
-    fs.writeFileSync(configPath, yaml.safeDump(config, {skipInvalid: true}));
+]))
+.default(self => ({
+  storageService: "mongoose"
+}))
+.prompt(self => ([
+  {
+    if: {
+      fileNotFound: self.serviceConfigMainYML
+    },
+    type: 'input',
+    name: 'storageService',
+    message: "storage service",
+    default: self.defaultConfig.storageService,
+    validate: function(value) {
+      return ['mongoose'].includes(value);
+    }
   }
+]))
+.run()
 
-  // enable service
-  console.log(`creating service alias: ${servicePath}`);
-  if (!fs.existsSync(serviceDir)) {
-    fs.mkdirSync(serviceDir, {recursive: true});
-  }
-  if (!fs.existsSync(servicePath)) {
-    fs.writeFileSync(servicePath, `module.exports = require('${moduleName}')`);
-  }
-};
-
-if (!skipAutoconf) {
-  const packageJson = require('./package.json');
-  const serviceName = 'authorize';
-  const moduleName = packageJson.name;
-  const defaultConf = {
-    storageService: "mongoose"
-  };
-
-  if (!skipPrompt) {
-    const questions = [
-      {
-        type: 'input',
-        name: 'storageService',
-        message: "storage service",
-        default: defaultConf.storageService,
-        validate: function(value) {
-          return ['mongoose'].includes(value);
-        }
-      }
-    ];
-
-    inquirer.prompt(questions).then(conf => {
-      generate(serviceName, moduleName, conf);
-    });  
-  } else {
-    generate(serviceName, moduleName, defaultConf);
-  }
-}
